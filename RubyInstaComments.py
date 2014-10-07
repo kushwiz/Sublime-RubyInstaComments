0da88baa1f3eb0d4b76611d36e09ad78f8093636
Kushal Bhandari <kushal.bhandari@tcs.com>
2014-10-07 18:25:52 +0530

    Generating comment based on selected controller action's first line
     * Select only the line which reads like "def <method_name>"
       This would replace the tags with the method's name. Just like how
       handlebars would work.
    
     * Fixed file path issue
    
     * Fixed shortcuts issue

--- RubyInstaComments.py
+++ RubyInstaComments.py
@@ -1,19 +1,34 @@
 import sublime, sublime_plugin
+import sys
+import re
+from os.path import join
+
+PLUGIN_NAME = "RubyInstaComments"
+
+plugin_dir = join(sublime.packages_path(), PLUGIN_NAME)
 
 """
 Command to stick in comments for a controller action
 """
-class RcaCommand(sublime_plugin.TextCommand):
+class RubyActionCommentCommand(sublime_plugin.TextCommand):
   def run(self, edit):
-    comment = file("rca.comment").read()
+    comment = file(plugin_dir+"/rca.comment").read()
     pos = self.view.sel()[0].begin()
+    method_defn = self.view.substr(self.view.sel()[0])
+    action_name = self.extract_method_name(method_defn)
+    comment = comment.replace("<action_name>", action_name)
     self.view.insert(edit, pos, comment)
+
+  def extract_method_name(self, method_defn):
+    p = re.compile("def (.*)")
+    return p.search(method_defn).group(1)
 
 """
 Command to stick in comments for a model function
 """
-class RmfCommand(sublime_plugin.TextCommand):
+class RubyMethodCommentCommand(sublime_plugin.TextCommand):
   def run(self, edit):
-    comment = file("rmf.comment").read()
+    comment = file(plugin_dir+"/rmf.comment").read()
     pos = self.view.sel()[0].begin()
     self.view.insert(edit, pos, comment)
+
